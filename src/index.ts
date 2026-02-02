c2db41cae34996843c75c3562235c35166b9abb4
Hleb Matsiash <hmatsiash@gmail.com>
2026-02-02 10:43:26 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Basic TypeScript playground entry file
+const msg: string = "Happy developing ✨";
+console.log(msg);
+
+const sum = (a: number, b: number): number => a + b;
+console.log("2 + 3 =", sum(2, 3));
