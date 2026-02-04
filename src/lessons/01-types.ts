7d891e780523ded549328de0e2f784f3b124488d
Hleb Matsiash <hmatsiash@gmail.com>
2026-02-04 22:34:03 +0100

    feat: add lesson 01 (basic types)

---
+++ src/lessons/01-types.ts
@@ -0,0 +1,10 @@
+// Lesson 01: Basic Types
+
+let username: string = "Hleb";
+let age: number = 30;
+let isActive: boolean = true;
+
+let tags: string[] = ["qa", "playwright", "typescript"];
+let scores: Array<number> = [10, 20, 30];
+
+console.log({ username, age, isActive, tags, scores });
