5989bbb415088c7ef00021cf046cf2cbc4ebfe26
Hleb Matsiash <hmatsiash@gmail.com>
2026-02-06 15:02:33 +0100

    feat: add lesson 01 (basic types)

--- src/lessons/01-types.ts
+++ src/lessons/01-types.ts
@@ -1,10 +1,17 @@
 // Lesson 01: Basic Types
 
 let username: string = "Hleb";
+let username_2 = "Hleb_2";
+username_2 = "Tom_2"
+
+const CITY = "Warsaw";
+
 let age: number = 30;
 let isActive: boolean = true;
 
 let tags: string[] = ["qa", "playwright", "typescript"];
+let tags_2= ["qa", "playwright", "typescript"];
 let scores: Array<number> = [10, 20, 30];
+const values = [1, "two", 3];
 
 console.log({ username, age, isActive, tags, scores });
